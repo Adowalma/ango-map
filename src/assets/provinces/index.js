ee196c5af715d4e6e696f5f87f7bfb007ef10ad5
AdowalmaAdao <adowalma.adao@selenium.ao>
2025-08-17 23:49:58 +0100

    2# content style

---
+++ src/assets/provinces/index.js
@@ -0,0 +1,36 @@
+import bengo from './bengo.png'
+import cabinda from './cabinda.png'
+import cuando from './cuando.png'
+import cuanzaNorte from './cuanza-norte.png'
+import cuanzaSul from './cuanza-sul.png'
+import cubango from './cubango.png'
+import cunene from './cunene.png'
+import icolo from './icolo-e-bengo.png'
+import luanda from './luanda.png'
+import lundaNorte from './lunda-norte.png'
+import lundaSul from './lunda-sul.png'
+import malange from './malange.png'
+import moxico from './moxico.png'
+import moxicoLeste from './moxico-leste.png'
+import uige from './uige.png'
+import zaire from './zaire.png'
+
+
+export{
+  bengo,
+  cabinda,
+  cuando,
+  cuanzaNorte,
+  cuanzaSul,
+  cubango,
+  cunene,
+  icolo,
+  luanda,
+  lundaNorte,
+  lundaSul,
+  malange,
+  moxico,
+  moxicoLeste,
+  uige,
+  zaire,
+}
